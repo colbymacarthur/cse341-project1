ec95596afb8012fc7424cb345fc32e5ca2b11866
Colby MacArthur <colby.mac89@gmail.com>
2024-09-23 10:54:21 -0600

    Setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongodb = require('./data/database.js'); 
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+        process.exit(1);
+    }
+    else {app.listen(port, () => {console.log(`Database connected and listening on port ${port}`)});}
+});
