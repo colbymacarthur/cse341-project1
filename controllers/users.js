4ab80a831b8bf25948afead5e304bf027ef914f2
Colby MacArthur <colby.mac89@gmail.com>
2024-09-27 13:17:08 -0600

    Week 2 push

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    //#swagger.tags=[Hello World]
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+    //#swagger.tags=[Hello World]
     const userId = ObjectId.createFromHexString(req.params.id);
     console.log(userId);
     const result = await mongodb.getDatabase().db().collection('contacts').find({_id: userId});
@@ -19,7 +21,56 @@ const getSingle = async (req, res) => {
     })
 };
 
+const createUser = async (req, res) => {
+    //#swagger.tags=[Hello World]
+    const user = {
+        name: req.body.name,
+        last: req.body.last,
+        email: req.body.email,
+        color: req.body.color,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user); 
+    if (response.acknowledged) {
+        res.status(201).json(response);
+    } else {
+        res.status(500).json(response.error || 'Unable to create user');
+    }
+};
+
+const updateUser = async (req, res) => {
+    //#swagger.tags=[Hello World]
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const user = {
+        name: req.body.name,
+        last: req.body.last,
+        email: req.body.email,
+        color: req.body.color,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({_id: userId}, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || {message: 'Unable to update user'});
+    }
+};
+
+const deleteUser = async (req, res) => {
+    //#swagger.tags=[Hello World]
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: userId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || {message: 'Unable to delete user'});
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const mongodb = require('./data/database');
+const bodayParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.use(bodayParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+})
 
 app.use('/', require('./routes/index'));
 
